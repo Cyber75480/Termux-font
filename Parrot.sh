04c8cb0dc238066db2a1a4a8e28ac70cabbdb119
Cybergamer75480 <c7887964@gmail.com>
2026-03-10 19:31:46 +0530

    Update prompt markers in Parrot.sh script

--- Parrot.sh
+++ Parrot.sh
@@ -4,14 +4,16 @@ echo "Installing Parrot Style..."
 
 cp ~/.bashrc ~/.bashrc.backup 2>/dev/null
 
+sed -i '/# >>> CYBER75 PROMPT START >>>/,/# <<< CYBER75 PROMPT END <<</d' ~/.bashrc
+
 cat >> ~/.bashrc <<'EOF'
 
-# >>> CYBER75 PARROT PROMPT START >>>
+# >>> CYBER75 PROMPT START >>>
 
 export PS1="\[\e[1;36m\]Cyber75\[\e[0m\]@\[\e[1;35m\][parrot:\w]\[\e[0m\]$ "
 echo -ne "\e[1;32m"
 
-# <<< CYBER75 PARROT PROMPT END <<<
+# <<< CYBER75 PROMPT END <<<
 
 EOF
 
