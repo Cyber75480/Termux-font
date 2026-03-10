e58428e6b0478a581dea5bf1bd1c7f9c8d432af6
Cybergamer75480 <c7887964@gmail.com>
2026-03-10 19:27:27 +0530

    Update prompt markers in Arch linux.sh script

--- Arch linux.sh
+++ Arch linux.sh
@@ -4,16 +4,16 @@ echo "Installing Arch Style..."
 
 cp ~/.bashrc ~/.bashrc.backup 2>/dev/null
 
-sed -i '/# >>> CYBER75/,/# <<< CYBER75/d' ~/.bashrc
+sed -i '/# >>> CYBER75 PROMPT START >>>/,/# <<< CYBER75 PROMPT END <<</d' ~/.bashrc
 
 cat >> ~/.bashrc <<'EOF'
 
-# >>> CYBER75 ARCH PROMPT START >>>
+# >>> CYBER75 PROMPT START >>>
 
 export PS1="\[\e[1;36m\]Cyber75\[\e[0m\]@\[\e[1;35m\][arch:\w]\[\e[0m\]$ "
 echo -ne "\e[1;32m"
 
-# <<< CYBER75 ARCH PROMPT END <<<
+# <<< CYBER75 PROMPT END <<<
 
 EOF
 
